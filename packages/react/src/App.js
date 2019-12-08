b028472a02ab3fecdd660cfc8ff79b25556b12ae
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-12-07 23:56:11 -0500

    react scss, html

--- packages/react/src/App.js
+++ packages/react/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+    <section>
+    <h1>assignment <small>by ryanspice for backbase</small></h1>
+    <p>Create an app that is capable of importing the attached MOCK_DATA.csv and visualize it on the screen.</p>
+    </section>
+    <section>
+    <h3>angular</h3>
+    <p>This page was created using "cdk-virtual-scroll-viewport" with the latest Angular, Material, and Webpack + 'csv-loader'.</p>
+    </section>
+    <section>
+    <h4>File: <span id="file">./MOCK_DATA.csv</span></h4>
+    </section>
+    <ol></ol>
+    <app-table ></app-table>
+    </main>
   );
 }
 
