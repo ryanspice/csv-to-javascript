633f8cdf1004e00fee3144da40c17745b99e59f9
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-12-07 21:12:23 -0500

    npx test

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const cp = require('child_process');
-cp.execSync('yarn csv-to-javascript --')
+const rootDir = path.join(__dirname, '..');
+const packagesDir = path.join(rootDir, 'packages');
+/*
+cp.execSync('yarn csv-to-javascript --',
+    {
+        cwd: rootDir,
+        stdio: 'inherit',
+    })
+
+ */
 //require('./packages/')
+const csvScriptPath = path.join(packagesDir, 'csv-to-javascript', 'index.js');
+cp.execSync(
+    `node ${csvScriptPath} ${args.join(' ')} --scripts-version="${scriptsPath}"`,
+    {
+        cwd: rootDir,
+        stdio: 'inherit',
+    }
+);
